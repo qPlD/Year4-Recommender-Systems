f2dfe2a26bb55e06ef4263fb27af88086295696a
qpld <2262655d@student.gla.ac.uk>
2019-10-15 00:27:29 +0100

    Used Spotlight to build basic system & succesfully visualise it using tsne

--- basic_recom_SL.py
+++ basic_recom_SL.py
@@ -1,25 +1,85 @@
 '''
 Base code is from https://github.com/maciejkula/spotlight
 '''
+import numpy as np
+import pylab
 from spotlight.cross_validation import random_train_test_split
 from spotlight.datasets.movielens import get_movielens_dataset
 from spotlight.evaluation import rmse_score
 from spotlight.factorization.explicit import ExplicitFactorizationModel
+from spotlight.interactions import Interactions
+from tsne_python.tsne import tsne
 
 #Datasets options: https://grouplens.org/datasets/movielens/
 dataset = get_movielens_dataset(variant='100K')
 print(dataset)
 
+#Lists of all interactions where userIds[x], movieIds[x] and ratings[x] correspond
+userIds = dataset.user_ids
+movieIds = dataset.item_ids
+ratings = dataset.ratings
+
+#Number of DIFFERENT users and movies per list (!= length of lists)
+numUsers = dataset.num_users
+numMovies = dataset.num_items
+
+#print(userIds,numUsers,len(userIds)        944  100000
+#print(movieIds,numMovies,len(movieIds))    1683 100000
+#print(ratings,len(ratings))                     100000
 #Split the dataset to evaluate the model
 train, test = random_train_test_split(dataset)
 print('Split into \n {} and \n {}.'.format(train, test))
 
-
+# Works for 5 iterations but crashes tsne for 10
 model = ExplicitFactorizationModel(n_iter=5)
 model.fit(train, verbose=True)
+
+# METHOD 1 (to get rating prediction matrix)
+# Register the unique Ids of all items (movies)
+'''
+uniqueMovieIds = np.empty(numMovies,)
+for Id in movieIds:
+    if Id not in uniqueMovieIds:
+        np.append(uniqueMovieIds,Id)
+        #uniqueIds += Ids
+print(userIds.shape)
+print(uniqueMovieIds.shape)
+
+predictions = model.predict(uniqueMovieIds)
+print(predictions)
+'''
+# METHOD 2
+'''
+predictions = np.dot(userIds, movieIds)
+print (predictions)
+'''
+# METHOD 3
+#Proof to verify that predictions for any user are made for all items
+'''
+user196 = model.predict(196)
+user186 = model.predict(186)
+user22 = model.predict(22)
+user13 = model.predict(13)
+
+print(user196,len(user196))
+print(user186,len(user186))
+print(user22,len(user22))
+print(user13,len(user13))
+'''
+predictions = np.empty((numUsers,numMovies))
+for i in range (numUsers):
+    predictions[i,:] = model.predict(i)
+
+print("Shape of Prediction matrix:",predictions.shape)
+#print(predictions[1])
+
+# We take the transpose for tsne formatting (should be more rows than columns)
+Y = tsne(500,predictions.T, 2, 50, 20.0)
+pylab.scatter(Y[:, 0], Y[:, 1], 20)
+pylab.show()
 
 #Measure the model's effectiveness (how good predictions are):
 rmse = rmse_score(model, test)
 train_rmse = rmse_score(model, train)
 test_rmse = rmse_score(model, test)
-print('RMSE {:.3f}, Train RMSE {:.3f}, test RMSE {:.3f}'.format(rmse,train_rmse, test_rmse))
+print('Train RMSE {:.3f}, test RMSE {:.3f}'.format(train_rmse, test_rmse))
