6030a29b84d79b5e927847d654fe54836e42b191
qpld <2262655d@student.gla.ac.uk>
2019-10-23 20:02:01 +0100

    Testing output graph with various perplexity settings. Updated Sources

--- basic_recom_SL.py
+++ basic_recom_SL.py
@@ -12,27 +12,28 @@ from tsne_python.tsne import tsne
 
 #Datasets options: https://grouplens.org/datasets/movielens/
 dataset = get_movielens_dataset(variant='100K')
-print(dataset)
 
 #Lists of all interactions where userIds[x], movieIds[x] and ratings[x] correspond
 userIds = dataset.user_ids
-movieIds = dataset.item_ids
-#ratings = dataset.ratings
-
+movieIds = dataset.item_ids # range from 0 to 1682
 #Number of DIFFERENT users and movies per list (!= length of lists)
 numUsers = dataset.num_users
 numMovies = dataset.num_items
 
-#print(userIds,numUsers,len(userIds)        944  100000
-#print(movieIds,numMovies,len(movieIds))    1683 100000
+#ratings = dataset.ratings
+
+#print(numUsers,len(userIds)        944  100000
+#print(numMovies,len(movieIds))    1683 100000
 #print(ratings,len(ratings))                     100000
 #Split the dataset to evaluate the model
 train, test = random_train_test_split(dataset)
 print('Split into \n {} and \n {}.'.format(train, test))
 
 # Works for 5 iterations but crashes tsne for 10
-model = ExplicitFactorizationModel(n_iter=20)
+model = ExplicitFactorizationModel(n_iter=50)
 model.fit(train, verbose=True)
+
+
 
 
 # METHOD 2
@@ -40,25 +41,24 @@ model.fit(train, verbose=True)
 predictions = np.dot(userIds, movieIds)
 print (predictions)
 '''
-# METHOD 3
-#Proof to verify that predictions for any user are made for all items
-'''
-user196 = model.predict(196)
-user186 = model.predict(186)
-user22 = model.predict(22)
-user13 = model.predict(13)
+#predictions for any user are made for all items, matrix has shape (944, 1683)
 
-print(user196,len(user196))
-print(user186,len(user186))
-print(user22,len(user22))
-print(user13,len(user13))
-'''
 predictions = np.empty((numUsers,numMovies))
 for i in range (numUsers):
     predictions[i,:] = model.predict(i)
 
-print("Shape of Prediction matrix:",predictions.shape)
-#print(predictions[1])
+
+
+# Each latent factor vector has 32 entries, type is torch tensor.
+user0_LF = model._net.user_embeddings.weight[0]
+item0_LF = model._net.item_embeddings.weight[0]
+
+#dottest = np.dot(user0_LF.T,item0_LF)
+#print(dottest, dottest.shape)
+#Find way to add categories for items (use as labels).
+
+#Project user into graph: user latent factor (dot product) item latent factor
+
 
 #Measure the model's effectiveness (how good predictions are):
 rmse = rmse_score(model, test)
@@ -67,9 +67,23 @@ test_rmse = rmse_score(model, test)
 print('Train RMSE {:.3f}, test RMSE {:.3f}'.format(train_rmse, test_rmse))
 
 # We take the transpose for tsne formatting (should be more rows than columns)
-Y = tsne(200,predictions.T, 2, 4, 20.0)
+# Y is a numpy array with shape (1683,2)
+Y = tsne(800,predictions.T, 2, 4, 100.0)
 
-pylab.scatter(Y[:, 0], Y[:, 1], 20)
+
+'''
+pylab.plot(x='Items',
+           y='Users',
+           data=Y,
+           fit_reg=False,
+           legend=True,
+           size=9,
+           hue='Label')
+pylab.title('Overall result for user/item ratings',weight='bold').set_fontsize('16')
+
+
+'''
+pylab.scatter(Y[:, 0], Y[:, 1], 10 ,'red','o')
 pylab.show()
 
 
